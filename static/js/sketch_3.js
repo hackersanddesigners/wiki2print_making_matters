f227751c770bef7d94942138b7774175a5082959
Heerko <heerko@swummoq.net>
2022-03-14 10:20:43 +0100

    new tryout sketch

---
+++ static/js/sketch_3.js
@@ -0,0 +1,80 @@
+function renderSketch(page, num, total, numChapters, currChapter){
+	// get all the terms (from the TOC) and wrap them in a <mark></mark> tag
+	let isRight = page.element.classList.contains("pagedjs_right_page"); // for positioning below
+	const toc_items = document.querySelectorAll("li:not(.tocsection-1,li.toclevel-1:last-of-type) .toclevel-2 > a > .toctext"); // get all terms defined in 
+	const ps = page.element.querySelectorAll('.pagedjs_pages p'); // paragraphs
+	let strings = []; 
+	for(let i = 0; i < toc_items.length; i++ ){
+		strings.push(toc_items[i].innerText.trim());
+	}
+	let str = strings.join("|"); // orrrrr
+	for( let i = 0; i < ps.length; i++ ) {
+		let p = ps[i];
+		let re = new RegExp(`(${str})([\s\.<])`, "gim"); // match "artistic research", but not "artistic researchers"
+		let text = p.innerHTML;
+		text = text.replace(re, (match, p1, p2) => { return "<mark>"+p1+"</mark>"+p2});
+		p.innerHTML = text;
+	}
+	// To convert term text to unique color
+	String.prototype.hashCode = function() {
+		var hash = 0, i, chr;
+		if (this.length === 0) return hash;
+		for (i = 0; i < this.length; i++) {
+			chr   = this.charCodeAt(i);
+			hash  = ((hash << 5) - hash) + chr;
+			hash |= 0; // Convert to 32bit integer
+		}
+		return hash;
+	};
+
+	const s = (sketch) => {
+		let canvas, el;
+		let dimensions = {};
+		
+		sketch.setup = () => {
+			el = page.element;
+			canvas = sketch.createCanvas(el.offsetWidth, el.offsetHeight, sketch.SVG);
+			canvas.parent(el);
+			canvas.position(0, 0);
+			canvas.style("z-index", 0);
+			sketch.ellipseMode(sketch.CENTER);
+			dimensions.bleed = sketch.getDimensions('bleed');
+			dimensions.margin = sketch.getDimensions('margin');
+			if(!page.element.querySelector("h1")){
+
+			}
+			let left = dimensions.bleed.left + 20 ;// + dimensions.margin.left;
+			if (isRight) {
+				left = sketch.width - dimensions.bleed.right - 20; // - dimensions.margin.right;
+			} 
+			let marks = el.querySelectorAll("mark");
+			sketch.noStroke();
+			
+			for( let i = 0; i < marks.length; i++ ) {
+				let mark = marks[i];
+				let text = mark.innerText;
+				let hash = text.hashCode();
+				let r = (hash & 0xFF0000) >> 16;
+				let g = (hash & 0x00FF00) >> 8;
+				let b = hash & 0x0000FF;
+				sketch.fill(r,g,b);
+				mark.style.backgroundColor = 'rgba(' + r  + "," +  g + "," +  b + ')';
+				let top = mark.offsetTop;
+				sketch.ellipse( left, dimensions.bleed.top + dimensions.margin.top + top + 8 , 10 ); // magic 8 = baseline = 12pt = 16px / 2
+			}
+		};
+
+		sketch.getDimensions = (area) => {
+			let dir = ['top', 'bottom', 'right', 'left'];
+			let ret = {};
+			for( let i = 0; i < 4; i++) {
+				let b = el.querySelector('.pagedjs_' + area + '-' + dir[i]);
+				ret[dir[i]] = (i < 2 ) ? b.clientHeight : b.clientWidth;
+			}
+			return ret;
+		}
+		// sketch.draw = () => {};
+	};
+
+	let myp5 = new p5(s); 
+}
